e8b1326b9663ad059bf701fed8e3be51a65891f5
Lior Schwartz <liorsmail89@gmail.com>
2020-09-28 23:13:37 +0300

    end of day commit 28-9-2020. still work to be done

--- src/main.cpp
+++ src/main.cpp
@@ -4,43 +4,93 @@
 #define yawPin    9
 #define pitchPin  10
 
-#define LDR_topRight      A0
-#define LDR_topLeft       A1
-#define LDR_bottomRight   A2
-#define LDR_bottomLeft    A3
+#define LDR_topRight        A0
+#define LDR_topLeft         A1
+#define LDR_bottomRight     A2
+#define LDR_bottomLeft      A3
 
-#define pitchLoopFreq     100 //hertz
-#define yawLoopFreq       100 //hertz
-#define yaw_dt_us         1000000 / yawLoopFreq
-#define pitch_dt_us       1000000 / pitchLoopFreq
+#define controlLoopFreq      10                         //hertz
+#define controlLoop_dt       1000 / controlLoopFreq     //milliseconds
+#define invertedYawLoop     false
+#define invertedPitchLoop   false
 
 
-Servo yawSer;
-Servo pitchSer;
+Servo yawServo;
+Servo pitchServo;
+
+unsigned long int ControlLoopTimer = 0;
+double yaw_controlEffort = 0;
+double pitch_controlEffort = 0;
 
 
 void setup() {
-  yawSer.attach(yawPin);
-  pitchSer.attach(pitchPin);
+  yawServo.attach(yawPin);
+  pitchServo.attach(pitchPin);
 
 }
 
 void loop() 
 {
-  
+  if(millis()-ControlLoopTimer>=controlLoop_dt)
+  {
+    controlLoop();
+  }
   // put your main code here, to run repeatedly:
 }
 
-int yaw_Controller()
+void controlLoop()
 {
+  ControlLoopTimer=millis();
+  yaw_Controller();
+  pitch_Controller();
+  yawServo.writeMicroseconds(yaw_controlEffort);
+  pitchServo.writeMicroseconds(pitch_controlEffort);
+}
 
+void yaw_Controller()
+{
+  double Ki = 1;
+  double deadZone = 10;
+  double r = 0;
+  double y = (double)(analogRead(LDR_topRight)+analogRead(LDR_bottomRight)-analogRead(LDR_topLeft)-analogRead(LDR_bottomLeft));
+  double du = Ki*((r-y)/100);
+
+  if(invertedYawLoop==true)
+  {
+    du*=-1;
+  }
+
+  if(yaw_controlEffort>1000 && yaw_controlEffort<2000)
+  {
+    yaw_controlEffort += du;
+  }
+  else
+  {
+    // this function acts as saturation
+  }
 }
 
 int pitch_Controller()
 {
-  
-  double r = (double) delta
-  double Kp = 1
-  u = Kp*r
+  double Kp = 1;
+  double deadZone = 10;
+  double r = 0;
+  double y = (double)(analogRead(LDR_topRight)+analogRead(LDR_topLeft)-analogRead(LDR_bottomRight)-analogRead(LDR_bottomLeft));
 
+  double du = Ki*((r-y)/100);
+
+  if(invertedYawLoop==true)
+  {
+    du*=-1;
+  }
+
+  if(yaw_controlEffort>1000 && yaw_controlEffort<2000)
+  {
+    yaw_controlEffort += du;
+  }
+  else
+  {
+    // this function acts as saturation
+  }
 }
+
